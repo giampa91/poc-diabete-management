f3c81e1e7e8519f87da5ce5bb3315a140321d579
giampaolo <giampaolo.orru@paranor.ch>
2024-11-14 20:46:39 +0100

    template form

--- frontend/diabete-management/app/index.tsx
+++ frontend/diabete-management/app/index.tsx
@@ -1,9 +1,19 @@
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import {Text, StyleSheet, View, TextInput, Button, Alert} from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text>this is the form</Text>
+      <TextInput placeholder="insulin type" />
+      <TextInput placeholder="glycemia" />
+      <TextInput placeholder="carbohydrate" />
+      <TextInput placeholder="dose" />
+      <TextInput placeholder="userId" />
+      <Button
+          title="Send data"
+          onPress={() => Alert.alert('Simple Button pressed')}
+        />
     </View>
   );
 }
