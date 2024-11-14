d421a8840190526470753782add511b2e4cc58b1
giampaolo <giampaolo.orru@paranor.ch>
2024-11-14 14:41:01 +0100

    insert base react native frontend

---
+++ frontend/diabete-management/app/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Hello Giampaolo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
