851d3bf44896d49c6e36f8c21d2561c5e3c621be
giampaolo <giampaolo.orru@paranor.ch>
2024-11-15 10:29:58 +0100

    frontend: insert simple input for glycemic data

--- frontend/diabete-management/app/index.tsx
+++ frontend/diabete-management/app/index.tsx
@@ -2,17 +2,32 @@ import React, {useState} from 'react';
 import {Text, StyleSheet, View, TextInput, Button, Alert} from 'react-native';
 
 export default function HomeScreen() {
+  const [insulinType, setInsulinType] = useState('');
+  const [glycemia, setGlycemia] = useState(0);
+  const [carbohydrate, setCarbohydrate] = useState(0);
+  const [dose, setDose] = useState(0);
+  const [userId, setUserId] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>this is the form</Text>
-      <TextInput placeholder="insulin type" />
-      <TextInput placeholder="glycemia" />
-      <TextInput placeholder="carbohydrate" />
-      <TextInput placeholder="dose" />
-      <TextInput placeholder="userId" />
+      <Text>insert glycemic data</Text>
+      <TextInput placeholder="insulin type" 
+        onChangeText={newInsulinType => setInsulinType(newInsulinType)}/>
+      <TextInput placeholder="glycemia" 
+        onChangeText={newGlycemia => setGlycemia(parseInt(newGlycemia))}/>
+      <TextInput placeholder="carbohydrate" 
+        onChangeText={newCarbohydrate => setCarbohydrate(parseInt(newCarbohydrate))}/>
+      <TextInput placeholder="dose"
+        onChangeText={newDose => setDose(parseInt(newDose))}/>
+      <TextInput placeholder="userId" 
+        onChangeText={newUserId => setUserId(parseInt(newUserId))}/>
       <Button
           title="Send data"
-          onPress={() => Alert.alert('Simple Button pressed')}
+          onPress={() => {
+              console.log(insulinType + " " + glycemia + " " + carbohydrate
+                + " " + dose + " " + userId
+              )
+            }
+          }
         />
     </View>
   );
